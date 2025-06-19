74f3d3bdbee2c5fe989bd9a4605861a71154978f
Rohit <rohityadav@Rohits-MacBook-Air.local>
2025-06-19 19:07:04 +0530

    added sidebar component

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,12 +1,68 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Menu from '../Icons/Menu'
+import MenuClose from '../Icons/MenuClose'
 
 function Navbar() {
+
+    const[menu, setMenu] = useState(false)
+    const [selected, setSelected] = useState(false)
   return (
-    <div className='flex w-11/12 justify-between  mx-auto mt-8'>
+
+    <div className='flex w-11/12 justify-between  mx-auto mt-8 relative'>
+
+            {/* <div className={`${menu ? "flex h-screen w-screen absolute bg-red-400":"hidden"}bg-red-400  -ml-15 -mt-8 h-screen w-screen s items-center justify-center`}>
+                <div className={`h-screen 
+                ${menu ? "w-[40%] flex origin-left  z-100 -ml-20 -mt-8":"w-0 hidden"}
+                 bg-black`}></div>
+            </div> */}
+
+            <div className={`${menu ? "flex h-screen w-screen bg-black/40 z-1000 absolute -mt-8 -ml-15 ":"hidden"}`}>
+                <div className='h-screen w-[25%] flex flex-col space-y-4 rounded-r-xl text-white bg-black p-6'>
+                    <div className='flex  p-4 justify-between rounded-2xl
+                      bg-zinc-800'>
+                        <div className='text-xl'>
+                            <div>VouchBase.</div>
+                        </div>
+                        <div onClick={() => setMenu(false)}
+                        className={`cursor-pointer`}>
+                            <MenuClose/>
+                        </div>
+                    </div>
+
+                    <div className='bg-zinc-800 p-4 rounded-2xl'>
+                        Profile
+                    </div>
+
+                    {/* MENUBAR-OPTION */}
+                    <div className='p-4 flex flex-col text-lg tracking-wide space-y-5'>
+                        <div className='rounded-2xl'>
+                            Testimonials
+                        </div>
+
+                        <div>
+                            Organisations
+                        </div>
+
+                        <div>
+                            Components
+                        </div>
+
+                        <div>
+                            Subscription
+                        </div>
+
+                        <div>
+                            Settings
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className='flex gap-1'>
                 <div className='outline-1 rounded-full px-3 py-1 '>Menu</div>
-                <div className='outline-1 rounded-full px-3 py-1'><Menu/></div>
+                <div onClick={() => setMenu((prev) => !prev)}
+                className='outline-1 rounded-full px-3 py-1 cursor-pointer'><Menu/></div>
             </div>
 
             <div className=' text-2xl'>
